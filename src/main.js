74a0a284cc197896d443d41637beca61c79898ad
Vadim Novash <novash@tut.by>
2020-08-19 16:29:31 +0300

    create render events by date template

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 import flatpickr from "flatpickr";
-import {renderHtmlElement, calendars} from './utils';
+import {renderHtmlElement} from './utils';
 import {createTripInfoTemplate as info} from './view/info';
 import {createTripCostTemplate as cost} from './view/cost';
 import {createTripSortsTemplate as sort} from './view/sort';
 import {createTripTabsTemplate as tabs} from './view/tabs';
 import {createTripFiltersTemplate as filter} from './view/filter';
 import {createTripDayItemTemplate as eventContainer} from './view/event-container';
-import {createEventTemplate as event} from './view/event';
 import {createTripFormTemplate as form} from './view/form';
 import {createEventTemplate} from './mocks/event';
 
-const EVENT_AMOUNT = 10;
+const EVENT_AMOUNT = 20;
 const tripContainer = document.querySelector(`.trip-main`);
 const tripInfo = tripContainer.querySelector(`.trip-info`);
 const tripControls = tripContainer.querySelector(`.trip-controls`);
@@ -18,20 +17,25 @@ const tripEventsContainer = document.querySelector(`.trip-events`);
 const tripDaysContainer = tripEventsContainer.querySelector(`.trip-days`);
 
 const events = new Array(EVENT_AMOUNT).fill().map(createEventTemplate);
-console.log(events);
+// console.log(events);
+const getEventsByDay = (arrayOfMocks) => {
+  const eventsList = new Map();
+  arrayOfMocks.forEach((eventItem) => {
+    const dateFrom = eventItem.dateFrom;
+    const eventDate = new Date(dateFrom.getFullYear(), dateFrom.getMonth(), dateFrom.getDate());
+    const key = eventDate.getTime();
+    if (!eventsList.has(key)) {
+      eventsList.set(key, []);
+    }
+    eventsList.get(key).push(eventItem);
+  });
+  return eventsList;
+};
 
 const tripStartDate = events[0].dateFrom;
 const tripEndDate = events[events.length - 1].dateTo;
 // set last event mpnth for comparison
 tripEndDate.setMonth(11);
-
-const addTripEvents = () => {
-  const container = document.querySelector(`.trip-events__list`);
-  const length = events.length - 1;
-  for (let i = 1; i <= length; i += 1) {
-    renderHtmlElement(container, event(events[i]), `beforeend`);
-  }
-};
 
 // render elems
 renderHtmlElement(tripInfo, info(tripStartDate, tripEndDate), `beforeend`);
@@ -40,8 +44,7 @@ renderHtmlElement(tripControls, tabs(), `beforeend`);
 renderHtmlElement(tripControls, filter(), `beforeend`);
 renderHtmlElement(tripDaysContainer, sort(), `beforebegin`);
 renderHtmlElement(tripDaysContainer, form(events[0]), `beforebegin`); // remove attribute from form() to have default form data
-renderHtmlElement(tripDaysContainer, eventContainer(), `beforeend`);
-addTripEvents();
+renderHtmlElement(tripDaysContainer, eventContainer(getEventsByDay(events)), `beforeend`);
 
 const flatpickrOptions = {
   enableTime: true,
--- src/view/event-container.js
+++ src/view/event-container.js
@@ -1,14 +1,22 @@
-export const createTripDayItemTemplate = () => {
-  return (`
-    <li class="trip-days__item  day">
-      <div class="day__info">
-        <span class="day__counter">1</span>
-        <time class="day__date" datetime="2019-03-18">MAR 18</time>
-      </div>
+import {createEventTemplate as event} from './event';
 
-      <ul class="trip-events__list">
+export const createTripDayItemTemplate = (arrayOfdays) => {
+  const result = [];
+  let counter = 0;
+  for (const [key, value] of arrayOfdays) {
+    const date = new Date(key);
+    result.push(`
+      <li class="trip-days__item  day">
+        <div class="day__info">
+          <span class="day__counter">${counter += 1}</span>
+          <time class="day__date" datetime="2019-03-18">${date.toLocaleString(`en-US`, {day: `numeric`, month: `short`})}</time>
+        </div>
 
-      </ul>
-    </li>
-  `);
+        <ul class="trip-events__list">
+          ${value.map((item) => event(item)).join(``)}
+        </ul>
+      </li>
+    `);
+  }
+  return result.join(``);
 };
