b6f82e420a88609c55222165049a6c6d2ecb0091
Vadim Novash <novash@tut.by>
2020-08-19 17:17:11 +0300

    fix event datetime attribute

--- src/view/event-container.js
+++ src/view/event-container.js
@@ -1,15 +1,20 @@
+
 import {createEventTemplate as event} from './event';
 
 export const createTripDayItemTemplate = (arrayOfdays) => {
   const result = [];
   let counter = 0;
   for (const [key, value] of arrayOfdays) {
-    const date = new Date(key);
+    const eventDate = new Date(key);
+    const year = eventDate.getFullYear();
+    const month = eventDate.toLocaleString(`en-US`, {day: `2-digit`});
+    const date = eventDate.getDate();
+
     result.push(`
       <li class="trip-days__item  day">
         <div class="day__info">
           <span class="day__counter">${counter += 1}</span>
-          <time class="day__date" datetime="2019-03-18">${date.toLocaleString(`en-US`, {day: `numeric`, month: `short`})}</time>
+          <time class="day__date" datetime="${year}-${month}-${date}">${eventDate.toLocaleString(`en-US`, {day: `numeric`, month: `short`})}</time>
         </div>
 
         <ul class="trip-events__list">
