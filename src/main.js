9e4478210b7c1678087a023d104e537f11d46fb0
Vadim Novash <novash@tut.by>
2020-08-26 18:03:47 +0300

    create events Boar presenter class
    - move logic from main to presenter

--- src/main.js
+++ src/main.js
@@ -1,25 +1,18 @@
-import flatpickr from "flatpickr";
-import {render, RenderPosition, replace} from './utils/render';
-import {getEventsByDay, flatpickrOptions} from './utils/event';
+import {render, RenderPosition} from './utils/render';
 import TripInfoView from './view/info';
 import TripCostView from './view/cost';
 import TripTabsView from './view/tabs';
 import FilterEventsView from './view/filter';
-import TripContainerView from './view/trip-container';
-import SortEventsView from './view/sort';
-import TripDaysContainerView from './view/trip-days-container';
-import TripDayItemView from './view/trip-day-item';
-import TripDayInfoView from './view/trip-day-info';
-import TripEventsListView from './view/trip-events-list';
-import EventView from './view/event';
 
-import EditFormView from './view/form';
+import TripContainerView from './view/trip-container';
 import NoEventsView from './view/no-events';
 
 import {createEventTemplate} from './mocks/event';
 import {generateFilter} from './mocks/filters';
 
-const EVENT_AMOUNT = 0;
+import BoardPresenter from "./presenter/board.js";
+
+const EVENT_AMOUNT = 20;
 const tripContainer = document.querySelector(`.trip-main`);
 const tripInfo = tripContainer.querySelector(`.trip-info`);
 const tripControls = tripContainer.querySelector(`.trip-controls`);
@@ -27,97 +20,34 @@ const pageMainElement = document.querySelector(`.page-main`);
 
 const events = new Array(EVENT_AMOUNT).fill().map(createEventTemplate);
 const filters = generateFilter(events);
-const eventsByDays = getEventsByDay(events);
-// console.log(eventsByDays)
+
 const tripStartDate = events.length ? events[0].dateFrom : ``;
 const tripEndDate = events.length ? events[events.length - 1].dateTo : ``;
-// set last event mpnth for comparison
-// tripEndDate.setMonth(11);
+// set last event month for comparison
+tripEndDate.setMonth(11);
 
 const tabsComponent = new TripTabsView();
 const filterComponent = new FilterEventsView(filters);
+
 const tripComponet = new TripContainerView();
-const tripDaysComponent = new TripDaysContainerView();
 const tripInfoComponent = new TripInfoView(tripStartDate, tripEndDate, events);
 const tripCostComponent = new TripCostView();
 
-// console.log(tripContainerElement)
 
-const renderEvent = (eventListContainer, event) => {
-  const eventComponent = new EventView(event);
-  const eventEditComponent = new EditFormView(event);
+render(tripInfo, tripInfoComponent.getElement(events), RenderPosition.BEFOREEND); // info
+render(tripInfo, tripCostComponent, RenderPosition.BEFOREEND); // cost
+render(tripControls, tabsComponent.getHeaderElement(), RenderPosition.BEFOREEND); // tabs header
+render(tripControls, tabsComponent, RenderPosition.BEFOREEND); // tabs
+render(tripControls, filterComponent.getHeaderElement(), RenderPosition.BEFOREEND); // filter header
+render(tripControls, filterComponent, RenderPosition.BEFOREEND); // filter
 
-  const replaceCardToForm = () => {
-    replace(eventEditComponent, eventComponent);
-    // eventComponent.getElement().replaceWith(eventEditComponent.getElement());
-  };
-
-  const replaceFormToCard = () => {
-    replace(eventComponent, eventEditComponent);
-    // eventEditComponent.getElement().replaceWith(eventComponent.getElement());
-  };
-  const setFlatPicker = () => {
-    const startDateEventField = flatpickr(`#event-start-time-1`, flatpickrOptions);
-    const endDateEventField = flatpickr(`#event-end-time-1`, flatpickrOptions);
-    return [startDateEventField, endDateEventField];
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.key === `Escape` || evt.key === `Esc`) {
-      evt.preventDefault();
-      replaceFormToCard();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  const onFormSubmit = () => {
-    eventEditComponent.setSubmitEditEventHandler(() => {
-      replaceFormToCard();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    });
-  };
-
-  eventComponent.setEditEventHandler(() => {
-    replaceCardToForm();
-    setFlatPicker();
-    onFormSubmit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-
-  render(eventListContainer, eventComponent, RenderPosition.BEFOREEND);
-};
-
-const renderTripDay = (date, arrayOfEvents, index) => {
-  const tripDayItemComponent = new TripDayItemView();
-  const tripDayInfoComponent = new TripDayInfoView(date, index);
-  const tripEventsListComponent = new TripEventsListView();
-
-  render(tripDaysComponent, tripDayItemComponent, RenderPosition.BEFOREEND);
-  render(tripDayItemComponent, tripDayInfoComponent, RenderPosition.BEFOREEND);
-  render(tripDayItemComponent, tripEventsListComponent, RenderPosition.BEFOREEND);
-  arrayOfEvents.forEach((event) => renderEvent(tripEventsListComponent, event));
-};
-
-// render common elems. For TripInfoView set third argument, wich
-render(tripInfo, tripInfoComponent.getElement(events), RenderPosition.BEFOREEND);
-render(tripInfo, tripCostComponent, RenderPosition.BEFOREEND);
-render(tripControls, tabsComponent.getHeaderElement(), RenderPosition.BEFOREEND);
-render(tripControls, tabsComponent, RenderPosition.BEFOREEND);
-render(tripControls, filterComponent.getHeaderElement(), RenderPosition.BEFOREEND);
-render(tripControls, filterComponent, RenderPosition.BEFOREEND);
-render(pageMainElement.firstElementChild, tripComponet, RenderPosition.AFTERBEGIN);
-render(tripComponet, tripComponet.getHeaderElement(), RenderPosition.AFTERBEGIN);
 // if no events was added, show a placeholder
+
+const boardPresenter = new BoardPresenter(pageMainElement);
+
 if (!events.length) {
   const noEventsPlaceholder = new NoEventsView();
   render(tripComponet, noEventsPlaceholder, RenderPosition.BEFOREEND); // no events placeholder
 } else {
-  const sortEventsComponent = new SortEventsView();
-  render(tripComponet, sortEventsComponent, RenderPosition.BEFOREEND);
-  render(tripComponet, tripDaysComponent, RenderPosition.BEFOREEND);
-
-  Array.from(eventsByDays).forEach(([key, value], index) => {
-    renderTripDay(key, value, index);
-  });
+  boardPresenter.init(events);
 }
---
+++ src/presenter/board.js
@@ -0,0 +1,113 @@
+import flatpickr from "flatpickr";
+import {render, RenderPosition, replace} from "../utils/render.js";
+import {getEventsByDay, flatpickrOptions} from '../utils/event.js';
+
+import TripContainerView from '../view/trip-container.js';
+import SortEventsView from '../view/sort.js';
+import TripDaysContainerView from '../view/trip-days-container.js';
+import TripDayItemView from '../view/trip-day-item.js';
+import TripDayInfoView from '../view/trip-day-info.js';
+import TripEventsListView from '../view/trip-events-list.js';
+import EventView from '../view/event.js';
+
+import EditFormView from '../view/form.js';
+
+export default class Board {
+  constructor(boardContainer) {
+    this._boardContainer = boardContainer;
+
+    this._tripComponent = new TripContainerView();
+    this._sortComponent = new SortEventsView();
+    this._tripDaysComponent = new TripDaysContainerView();
+  }
+
+  init(events) {
+    this._events = events.slice();
+
+    render(this._boardContainer.firstElementChild, this._tripComponent, RenderPosition.AFTERBEGIN);
+    render(this._tripComponent, this._tripComponent.getHeaderElement(), RenderPosition.AFTERBEGIN); // trip-events header
+
+    this._renderBoard();
+  }
+
+  _renderSort() {
+    render(this._tripComponent, this._sortComponent, RenderPosition.BEFOREEND);
+  }
+
+  _renderTripDaysContainer() {
+    render(this._tripComponent, this._tripDaysComponent, RenderPosition.BEFOREEND);
+  }
+
+  _renderEvent(eventsListContainer, event) {
+    const eventComponent = new EventView(event);
+    const eventEditComponent = new EditFormView(event);
+
+    const replaceCardToForm = () => {
+      replace(eventEditComponent, eventComponent);
+      // eventComponent.getElement().replaceWith(eventEditComponent.getElement());
+    };
+
+    const replaceFormToCard = () => {
+      replace(eventComponent, eventEditComponent);
+      // eventEditComponent.getElement().replaceWith(eventComponent.getElement());
+    };
+    const setFlatPicker = () => {
+      const startDateEventField = flatpickr(`#event-start-time-1`, flatpickrOptions);
+      const endDateEventField = flatpickr(`#event-end-time-1`, flatpickrOptions);
+      return [startDateEventField, endDateEventField];
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        evt.preventDefault();
+        replaceFormToCard();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      }
+    };
+
+    const onFormSubmit = () => {
+      eventEditComponent.setSubmitEditEventHandler(() => {
+        replaceFormToCard();
+        document.removeEventListener(`keydown`, onEscKeyDown);
+      });
+    };
+
+    eventComponent.setEditEventHandler(() => {
+      replaceCardToForm();
+      setFlatPicker();
+      onFormSubmit();
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+
+    render(eventsListContainer, eventComponent, RenderPosition.BEFOREEND);
+  }
+
+  _renderTripDay(date, arrayOfEvents, index) {
+    const tripDayItemComponent = new TripDayItemView();
+    const tripDayInfoComponent = new TripDayInfoView(date, index);
+    const tripEventsListComponent = new TripEventsListView();
+
+    render(this._tripDaysComponent, tripDayItemComponent, RenderPosition.BEFOREEND);
+    render(tripDayItemComponent, tripDayInfoComponent, RenderPosition.BEFOREEND);
+    render(tripDayItemComponent, tripEventsListComponent, RenderPosition.BEFOREEND);
+    arrayOfEvents.forEach((event) => this._renderEvent(tripEventsListComponent, event));
+  }
+
+  _renderTripEventList() {
+    this._eventsByDays = getEventsByDay(this._events.slice());
+
+    Array.from(this._eventsByDays).forEach(([key, value], index) => {
+      this._renderTripDay(key, value, index);
+    });
+  }
+
+  _renderBoard() {
+
+    this._renderSort();
+
+    this._renderTripDaysContainer();
+
+    this._renderTripEventList();
+  }
+}
